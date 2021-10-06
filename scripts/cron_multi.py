6270d590aa7bba83342f532f7c52414c4ef21542
Emilio Gozo <15209351+yohlime@users.noreply.github.com>
2021-10-06 14:39:08 +0800

    chore: remove test

--- scripts/cron_multi.py
+++ scripts/cron_multi.py
@@ -10,12 +10,10 @@ from parse_jtwc import proc_tc_data as get_jtwc
 from parse_t2k import proc_tc_data as get_t2k
 from make_shp import make_shp
 
-
-IS_TEST = False
 CONFIG = dotenv_values()
 
 
-def main(is_test=IS_TEST):
+def main():
     # Load TC information
     print("Loading TC information...")
     DATETIME_NOW = pd.to_datetime(datetime.now())
@@ -74,17 +72,7 @@ def main(is_test=IS_TEST):
     tc_code = (
         f"{CONFIG['TC_BASIN']}{CONFIG['TC_CY'].rjust(2, '0')}{CONFIG['TC_YEAR'][2:]}"
     )
-    if is_test:
-        jtwc_df = get_jtwc(
-            tc_code,
-            base_url="http://localhost:8000/",
-            timestamp=DATETIME_NOW,
-        )
-    else:
-        jtwc_df = get_jtwc(
-            tc_code,
-            raw_out_dir=raw_out_dir,
-        )
+    jtwc_df = get_jtwc(tc_code, raw_out_dir=raw_out_dir)
     if isinstance(jtwc_df, pd.DataFrame):
         c_date = jtwc_df.loc[jtwc_df["PosType"] == "c", "Date"].values
         if len(c_date) > 0:
@@ -99,15 +87,7 @@ def main(is_test=IS_TEST):
     # Get multilog from Typhoon2000
     print("Getting TC data from Typhoon2k...")
     tc_name = CONFIG["TC_NAME"]
-    if is_test:
-        t2k_df = get_t2k(
-            tc_name,
-            base_url="http://localhost:8000/",
-            exclude="JTWC",
-            timestamp=DATETIME_NOW,
-        )
-    else:
-        t2k_df = get_t2k(tc_name, exclude="JTWC", raw_out_dir=raw_out_dir)
+    t2k_df = get_t2k(tc_name, exclude="JTWC", raw_out_dir=raw_out_dir)
     if isinstance(t2k_df, pd.DataFrame):
         out_df = out_df.append(t2k_df, ignore_index=True)
     else:
