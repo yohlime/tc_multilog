fb05fe9d978a855eae9c833fad07764e87f2f8cb
Emilio Gozo <15209351+yohlime@users.noreply.github.com>
2021-10-10 18:57:45 +0800

    chore: clean up

--- scripts/make_shp.py
+++ scripts/make_shp.py
@@ -1,21 +1,12 @@
 from pathlib import Path
 import pandas as pd
-from geopandas import GeoDataFrame
-from shapely.geometry import Point, LineString
+from geopandas import GeoDataFrame, points_from_xy
+from shapely.geometry import LineString
 import argparse
 
 OUTPUT_DIR = Path("output/shp")
 
-
-def df_to_gdf(df, lat="Lat", lon="Lon"):
-    geometry = [Point(xy) for xy in zip(df[lon], df[lat])]
-    return GeoDataFrame(df, crs=4326, geometry=geometry)
-
-
-def pts_to_line(gdf, group_by):
-    gdf = gdf.groupby(group_by).filter(lambda x: len(x) > 1)
-    gdf = gdf.groupby(group_by)["geometry"].apply(lambda x: LineString(x.tolist()))
-    return GeoDataFrame(gdf.reset_index(), geometry="geometry", crs=4326)
+PROJ_CRS = 4326
 
 
 def add_radius(gdf, radius):
@@ -23,28 +14,34 @@ def add_radius(gdf, radius):
         gdf = gdf.dropna(subset=[radius])
         if gdf.shape[0] > 0:
             geometry = gdf.apply(lambda x: x.geometry.buffer(x[radius] / 111.0), axis=1)
-            return GeoDataFrame(gdf, geometry=geometry, crs=4326)
+            return GeoDataFrame(gdf, geometry=geometry, crs=PROJ_CRS)
     return None
 
 
 def make_shp(in_file, out_dir=OUTPUT_DIR):
     df = pd.read_csv(in_file)
-    row_to_insert = df[(df["Center"] == "JTWC") & (df["PosType"] == "c")].copy()
-    if row_to_insert.shape[0] == 1:
-        row_split_index = df[(df["Center"] == "JTWC") & (df["PosType"] == "c")].index.values[0]
-        row_to_insert["Center"] = "JTWC_forecast"
-        df.loc[(df["Center"] == "JTWC") & (df["PosType"] == "f"), "Center"] = "JTWC_forecast"
-        df2 = df.iloc[0 : row_split_index + 1].append(row_to_insert, ignore_index=True)
-        df = df2.append(df.iloc[row_split_index + 1 :], ignore_index=True)
+    for center_name in df["Center"].unique():
+        row_to_insert = df[(df["Center"] == center_name) & (df["PosType"] == "c")].copy()
+        if row_to_insert.shape[0] == 1:
+            row_split_index = df[(df["Center"] == center_name) & (df["PosType"] == "c")].index.values[0]
+            row_to_insert["Center"] = f"{center_name}_forecast"
+            df.loc[(df["Center"] == center_name) & (df["PosType"] == "f"), "Center"] = f"{center_name}_forecast"
+            df2 = df.iloc[0 : row_split_index + 1].append(row_to_insert, ignore_index=True)
+            df = df2.append(df.iloc[row_split_index + 1 :], ignore_index=True)
 
+    # df to gdf pts
+    geom = points_from_xy(df["Lon"], df["Lat"], crs=PROJ_CRS)
+    gdf_pts = GeoDataFrame(df.copy(), crs=PROJ_CRS, geometry=geom)
     _out_dir = out_dir / "track_pts"
     _out_dir.mkdir(parents=True, exist_ok=True)
-    gdf_pts = df_to_gdf(df)
     gdf_pts.to_file(_out_dir)
 
+    # connect the dots
+    gdf_lns = gdf_pts.copy().groupby("Center").filter(lambda x: len(x) > 1)
+    gdf_lns = gdf_lns.groupby("Center")["geometry"].apply(lambda x: LineString(x.tolist()))
+    gdf_lns = GeoDataFrame(gdf_lns.reset_index(), geometry="geometry", crs=PROJ_CRS)
     _out_dir = out_dir / "track_line"
     _out_dir.mkdir(parents=True, exist_ok=True)
-    gdf_lns = pts_to_line(gdf_pts, "Center")
     gdf_lns.to_file(_out_dir)
 
     gdf_rad = gdf_pts[gdf_pts["Center"] == "JTWC_forecast"].copy()
